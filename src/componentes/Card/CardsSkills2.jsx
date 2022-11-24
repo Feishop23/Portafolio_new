40e264fd3db91ffedb94f645d74d6a45bf37436c
Feiberth Portilla <feiberthportilla@mac-mini.mynetworksettings.com>
2022-11-23 20:15:55 -0500

    solo esta de darle desing al Proyecto

--- src/componentes/Card/CardsSkills2.jsx
+++ src/componentes/Card/CardsSkills2.jsx
@@ -5,19 +5,24 @@ const CardsSkills2 = () => {
     const cards = [
         {
           id: 1,
-          title: ' Feiberth',
-          description: 'lorem lalsjjflaslkfjasdlfjloaiksjfolaisdjfoiasdjf'
+          title: 'Rick and mort',
+          description: 'Find your favorite character from the rick and mort series and check out his or her full potential. '
         },
         {
           id: 2,
-          title: ' Josue',
-          description: 'lorem lalsjjflaslkfjasdlfjloaiksjfolaisdjfoiasdjf'
+          title: 'Weather',
+          description: 'Check the weather status from our application that uses your location in real time thanks to the use of APIs.'
         },
         {
           id: 3,
-          title: ' Portilla',
-          description: 'lorem lalsjjflaslkfjasdlfjloaiksjfolaisdjfoiasdjf'
+          title: ' E-commerce',
+          description: 'Come and try our online store layout where the resources used were also taken from the use of APIs.'
         },
+        {
+          id: 4,
+          title: 'Card random Generator',
+          description: 'Interact with these cards where the operation is to give you random values thanks to the use of API.'
+        }
       ]
   return (
     <div >
